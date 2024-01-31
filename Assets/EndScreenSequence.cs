c1ed25a5e0f021d935b08ff55f0540a7e943a254
Pau <pauguvi@gmail.com>
2024-01-31 17:15:54 +0100

    end screen

---
+++ Assets/EndScreenSequence.cs
@@ -0,0 +1,28 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScreenSequence : MonoBehaviour
+{
+    [SerializeField] private Image logo;
+    [SerializeField] private CanvasGroup creditsCanvasGroup;
+    [SerializeField] private CustomButton exitButton;
+
+    private void Start()
+    {
+        exitButton.Disable();
+        creditsCanvasGroup.alpha = 0f;
+
+        Sequence sequence = DOTween.Sequence();
+        sequence.AppendInterval(1f);
+        sequence.Append(logo.DOFade(1f, 1f));
+        sequence.AppendInterval(3f);
+        sequence.Append(creditsCanvasGroup.DOFade(1f, 1f));
+        sequence.AppendCallback(() => exitButton.Enable());
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
